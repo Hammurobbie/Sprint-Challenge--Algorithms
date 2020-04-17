ba5d4d93d62edb36e17176463a6c3dae5c26e847
Robbie Porter <rthomporter@gmail.com>
2020-04-17 11:53:40 -0500

    recursion

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,8 +5,27 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 
+count = 0
+
+
+def split(string):
+    print(string)
+    to_list = list(string)
+    del(to_list[0])
+    new_string = "".join(to_list)
+    return new_string
+
+
 def count_th(word):
+    global count
+    if len(word) <= 1:
+        print(count)
+        return count
+    elif word[0] == "t" and word[1] == "h":
+        count += 1
+    return count_th(split(word))
 
-    # TBC
 
-    pass
+count_th("abcthefthghith")
+
+# tests fail because global counter isn't resetting between function calls. If you test all cases individually, they all return the expected result
